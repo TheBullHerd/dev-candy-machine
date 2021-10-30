df8afa2f85e61dd6626716dbcd3b0a6b9b0d39d3
newcomt-lab <newcometcto729@gmail.com>
2021-10-30 12:05:27 +0900

    fixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path')
 var app = express()
 
 
-app.use(express.static(path.join(__dirname, 'out')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(80, function () {
   console.log('App listening on port 80!')
